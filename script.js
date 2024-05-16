76a6f0a5d30c97e7178c5f5a34820eff69864ffc
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-16 12:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let inputs = document.getElementsByClassName("code")
+//console.log(inputs)
+
+for(let i=0;i<inputs.length;i++){
+    inputs[i].addEventListener("click",(events)=>{
+        //console.log(events)
+        inputs[i].setAttribute("placeholder","")
+    })
+}
