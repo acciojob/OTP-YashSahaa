31b1b842925b06b574e9d1d051cd70e0b17ea912
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-16 12:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ for(let i=0;i<inputs.length;i++){
         inputs[i].setAttribute("placeholder","")
     })
 }
+for(let i=0;i<inputs.length;i++){
+    inputs[i].addEventListener("keyup",(events)=>{
+        //console.log(events)
+
+        if(events.key==="Backspace" && inputs[i].value==""){
+            if(i>0){
+                inputs[i].removeAttribute("autofocus")
+                inputs[i-1].setAttribute("placeholder","")
+                inputs[i-1].focus()
+            }
+        }
+        if(events.key.charCodeAt(0)>47 && events.key.charCodeAt(0)<58){
+            if(i<inputs.length-1){
+                inputs[i].removeAttribute("autofocus")
+                inputs[i+1].setAttribute("placeholder","")
+                inputs[i+1].focus()
+            }
+        }
+    })
+}
