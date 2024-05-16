4028a266fc814f559e413da75a07cf17cf01f5ff
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-16 13:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
 let inputs = document.getElementsByClassName("code")
 //console.log(inputs)
-
+window.onload = function() {
+  document.getElementById("code-1").focus();
+	document.getElementById("code-1").setAttribute("placeholder","")
+};
 for(let i=0;i<inputs.length;i++){
     inputs[i].addEventListener("click",(events)=>{
         //console.log(events)
@@ -14,14 +17,12 @@ for(let i=0;i<inputs.length;i++){
 
         if(events.key==="Backspace" && inputs[i].value==""){
             if(i>0){
-                inputs[i].setAttribute("focused","")
                 inputs[i-1].setAttribute("placeholder","")
                 inputs[i-1].focus()
             }
         }
         else if(events.key.charCodeAt(0)>47 && events.key.charCodeAt(0)<58){
             if(i<inputs.length-1){
-                //vinputs[i].removeAttribute("autofocus")
                 inputs[i+1].setAttribute("placeholder","")
                 inputs[i+1].focus()
             }
